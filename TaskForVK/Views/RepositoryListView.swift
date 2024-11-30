642eaad8b3b44ff43e63dc571c85321975fd2c88
bemadeveloper <akbermet1990@icloud.com>
2024-11-30 21:38:15 +0600

    Creating View

--- TaskForVK/Views/RepositoryListView.swift
+++ TaskForVK/Views/RepositoryListView.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RepositoryListView: View {
+    @StateObject var viewModel = RepositoryListViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.repositories) { repository in
+                RepositoryCell(repository: repository)
+            }
+            
+            if viewModel.isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+            }
+        }
+        .onAppear {
+            viewModel.loadMoreData()
+        }
+        .onChange(of: viewModel.repositories.count) { _ in
+            viewModel.loadMoreData()
+        }
+    }
+}
+
+struct RepositoryCell: View {
+    var repository: Repository
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(repository.name)
+                .font(.headline)
+            if let description = repository.description {
+                Text(description)
+                    .font(.subheadline)
+                    .lineLimit(2)
+            }
+        }
+    }
+}
