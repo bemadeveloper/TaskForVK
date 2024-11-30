60c4c50deae28baba8503db9d977045c2706f809
bemadeveloper <akbermet1990@icloud.com>
2024-11-30 21:37:59 +0600

    Creating ViewModel

--- TaskForVK/ViewModels/RepositoryListViewModel.swift
+++ TaskForVK/ViewModels/RepositoryListViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+
+class RepositoryListViewModel: ObservableObject {
+    @Published var repositories: [Repository] = []
+    @Published var isLoading: Bool = false
+    
+    private var currentPage = 1
+    private var repositoryService = RepositoryService()
+    
+    func loadMoreData() {
+        guard !isLoading else { return }
+        
+        isLoading = true
+        repositoryService.fetchRepositories(page: currentPage) { result in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                switch result {
+                case .success(let newRepositories):
+                    self.repositories.append(contentsOf: newRepositories)
+                    self.currentPage += 1
+                case .failure:
+                    // Handle error here
+                    break
+                }
+            }
+        }
+    }
+}
