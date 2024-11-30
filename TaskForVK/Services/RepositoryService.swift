e8d2043240d274566923014e3bd68018c062f18f
bemadeveloper <akbermet1990@icloud.com>
2024-11-30 21:37:14 +0600

    Creating Service and Storage

--- TaskForVK/Services/RepositoryService.swift
+++ TaskForVK/Services/RepositoryService.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class RepositoryService {
+    let baseURL = "https://api.github.com/search/repositories?q=swift&sort=stars&order=asc&page="
+    
+    func fetchRepositories(page: Int, completion: @escaping (Result<[Repository], Error>) -> Void) {
+        let url = URL(string: "\(baseURL)\(page)")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "Invalid data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(RepositoryResponse.self, from: data)
+                completion(.success(response.items))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct RepositoryResponse: Codable {
+    var items: [Repository]
+}
--- TaskForVK/Services/RepositoryStorage.swift
+++ TaskForVK/Services/RepositoryStorage.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RepositoryStorage {
+    let realm = try! Realm()
+    
+    func add(repository: Repository) {
+        try! realm.write {
+            realm.add(repository)
+        }
+    }
+    
+    func delete(repository: Repository) {
+        try! realm.write {
+            realm.delete(repository)
+        }
+    }
+    
+    func fetchRepositories() -> Results<Repository> {
+        return realm.objects(Repository.self)
+    }
+}
